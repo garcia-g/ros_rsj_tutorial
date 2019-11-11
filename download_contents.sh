2804225ab4d7b5d63ef15ad5983202be5cb4c5f1
takahasi <3263ta@gmail.com>
2019-11-11 20:48:40 +0900

    Update download script

--- download_contents.sh
+++ download_contents.sh
@@ -41,23 +41,44 @@ source /opt/ros/kinetic/setup.bash
     source devel/setup.bash && \
     cd src && \
     git clone https://github.com/gbiggs/rsj_tutorial_2017_ros_intro.git && \
+    cd .. && catkin_make)
+(mkdir -p crane_plus_ws/src && \
+    cd clane_plus_ws/src && rm -rf CMakeLists.txt && catkin_init_workspace && \
+    cd .. && catkin_make && \
+    source devel/setup.bash && \
+    cd src && \
     git clone https://github.com/gbiggs/crane_plus_arm.git && \
     git clone https://github.com/gbiggs/rsj_2017_pick_and_placer.git && \
+    cd .. && catkin_make)
+(mkdir -p block_finder_ws/src && \
+    cd block_finder_ws/src && rm -rf CMakeLists.txt && catkin_init_workspace && \
+    cd .. && catkin_make && \
+    source devel/setup.bash && \
+    cd src && \
     git clone https://github.com/Suzuki1984/rsj_2017_block_finder.git && \
     cd .. && catkin_make)
+(mkdir -p rsj_2017_application_ws/src && \
+    cd rsj_2017_application_ws/src && rm -rf CMakeLists.txt && catkin_init_workspace && \
+    cd .. && catkin_make && \
+    source devel/setup.bash && \
+    cd src && \
+    git clone https://github.com/gbiggs/rsj_2017_pick_and_placer.git && \
+    git clone https://github.com/Suzuki1984/rsj_2017_block_finder && \
+    git clone https://github.com/gbiggs/crane_plus_arm.git && \
+    cd .. && catkin_make)
 
-base_url=https://takahasi.github.io/ros_moveit_rsj_tutorial
-rm -rf docs && mkdir -p docs
-wkhtmltopdf $base_url/index.html docs/index.pdf
-wkhtmltopdf $base_url/linux_and_ros_install.html docs/linux_and_ros_install.pdf
-wkhtmltopdf $base_url/linux_basics.html docs/linux_basics.pdf
-wkhtmltopdf $base_url/manipulator_check.html docs/manipulator_check.pdf
-wkhtmltopdf $base_url/ros_basics.html docs/ros_basics.pdf
-wkhtmltopdf $base_url/manipulators_and_moveit.html docs/manipulators_and_moveit.pdf
-wkhtmltopdf $base_url/camera_check.html docs/camera_check.pdf
-wkhtmltopdf $base_url/image_processing_and_opencv.html docs/image_processing_and_opencv.pdf
-wkhtmltopdf $base_url/ros_useful_stuff.html docs/ros_useful_stuff.pdf
-wkhtmltopdf $base_url/full_application.html docs/full_application.pdf
+#base_url=https://takahasi.github.io/ros_moveit_rsj_tutorial
+#rm -rf docs && mkdir -p docs
+#wkhtmltopdf $base_url/index.html docs/index.pdf
+#wkhtmltopdf $base_url/linux_and_ros_install.html docs/linux_and_ros_install.pdf
+#wkhtmltopdf $base_url/linux_basics.html docs/linux_basics.pdf
+#wkhtmltopdf $base_url/manipulator_check.html docs/manipulator_check.pdf
+#wkhtmltopdf $base_url/ros_basics.html docs/ros_basics.pdf
+#wkhtmltopdf $base_url/manipulators_and_moveit.html docs/manipulators_and_moveit.pdf
+#wkhtmltopdf $base_url/camera_check.html docs/camera_check.pdf
+#wkhtmltopdf $base_url/image_processing_and_opencv.html docs/image_processing_and_opencv.pdf
+#wkhtmltopdf $base_url/ros_useful_stuff.html docs/ros_useful_stuff.pdf
+#wkhtmltopdf $base_url/full_application.html docs/full_application.pdf
 
 
 exit 0
